450b488bcef3b7aa9703b0580519be5fec81658f
blessing-michael <blessingmike334@gmail.com>
2023-04-21 10:34:38 -0700

    hompage edited

--- src/Follow.js
+++ src/Follow.js
@@ -2,33 +2,62 @@ import React from 'react'
 import { AiFillApple } from "react-icons/ai";
 
 function Follow() {
-  return (
-    <div className='followus'>
+return (
+    <section className='followus'>
         <div className="follow-div">
-        <img src="../images/foodapp.png" alt="" style={{width:"40%",  rotate: "-12deg"}}/>
- <div className="followus-info">
-
-
-<div className='playstore'>
-<p className='getapp'>Get the app !</p>
+        <div className='playstore'>
+ <p className='getapp'>Get the app !</p>
   <h3 className='limitless-h3'>Limitless convenience on-demand</h3>
 </div>
-<div className="playstore-img">
+<div className="follow-img">
+<img src="../images/foodapp.png" alt="food-app" className='follow-image'/> 
 
-<img src="../images/appstore.svg" alt="appstore" style={{width:"100px", marginBottom:"0", marginRight:"40px"}} />
+</div>
+
+<div className="playstore-img">
+    <img src="../images/appstore.svg" alt="appstore" className='app-image' />
+    <img src="../images/playstore.svg" alt="playstore" className='play-image' />
+</div>
+ {/* <div className="playstore-img"> 
+
+ <img src="../images/appstore.svg" alt="appstore" style={{width:"100px", marginBottom:"0", marginRight:"40px"}} />
   
   <img src="../images/playstore.svg" alt="playstore"  style={{width:"100px"}}/>
-</div>
- </div>
-
-
-
+  </div>  */}
 
         </div>
+
+    </section>
+)
+
+//   return (
+//     <div className='followus'>
+//         <div className="follow-div">
+
+//         
+//  <div className="followus-info">
+
+
+// <div className='playstore'>
+// <p className='getapp'>Get the app !</p>
+//   <h3 className='limitless-h3'>Limitless convenience on-demand</h3>
+// </div>
+// <div className="playstore-img">
+
+// <img src="../images/appstore.svg" alt="appstore" style={{width:"100px", marginBottom:"0", marginRight:"40px"}} />
+  
+//   <img src="../images/playstore.svg" alt="playstore"  style={{width:"100px"}}/>
+// </div>
+//  </div>
+
+
+
+
+//         </div>
   
        
-    </div>
-  )
+//     </div>
+//   )
 }
 
 export default Follow

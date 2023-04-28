60374ea45a7dfee83894280fcc0bbe68b0f08140
blessing-michael <blessingmike334@gmail.com>
2023-04-28 12:21:04 -0700

    restyling the navbar for responsiveness, nav component written afresh

--- src/Nav.js
+++ src/Nav.js
@@ -1,61 +1,134 @@
 import React, { useState } from 'react'
 import { CgProfile} from "react-icons/cg";
 import { HiBars4} from "react-icons/hi2";
+import { FaCartArrowDown } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
-function Nav(props) {
-    const {countcartItems}= props
-    const [showNav, setShowNav]= useState(false)
+function Nav() {
     const navigate= useNavigate()
-
-    
     const navcat= ()=>{
-        navigate("/menu")
-        setShowNav(false)
-    
-    }
-    const navhome= ()=>{
-        navigate("/")
-        setShowNav(false)
-    }
-    const navfaq= ()=>{
-        navigate("/faq")
-        setShowNav(false)
-    }
-    const navsupport= ()=>{
-        navigate("/support")
-        setShowNav(false)
-    }
-    const navcart= ()=>{
-        navigate("/cart")
-        setShowNav(false)
-    }
+                navigate("/menu")
+                
+            
+             }
+             const navhome= ()=>{
+                 navigate("/")
+              
+             }
+             const navfaq= ()=>{
+                 navigate("/faq")
+                
+             }
+             const navsupport= ()=>{
+                 navigate("/support")
+                
+            }
+            const navcart= ()=>{
+                        navigate("/cart")
+               
+                     }
+        
 
-    const Shownavbar=()=>{
-        setShowNav(!showNav)
-        console.log("clicked nav")
-    }
+    return(
+    <nav className='nav-name'>
+        <div className="nav-center">
+            <div className="nav-header">
+                <div className="logo">
+               <img src="./images/logo.svg" alt="" />
+              
 
+                </div>
+             
+               <div className="icons-grp">
+               <button className='navbar-btn'> <HiBars4/></button>
+                <div className="cart-div" onClick={navcart}>
+               <button className='cart-btn'> <FaCartArrowDown/><span className='cart-numspan'>0</span></button>
+              
+                </div>
 
-  return (
-   <header className='header'>
-  <button className='profile'><CgProfile/></button>
-    {/* <h3 className='home'>home</h3> */}
-
-
-    {showNav && <nav className='nav'>
-        <ul>
-            <li onClick={()=> navhome()}>Home</li>
+               </div>
+               
+            </div>
+            <div className="links-container show-container">
+                <ul className='links' >
+                <li onClick={()=> navhome()}>Home</li>
             <li onClick={()=> navcat()}>Menu</li>
-            <li onClick={()=> navfaq()}>FAQS</li>
-            <li onClick={()=> navsupport()}>Support</li>
-            <li onClick={()=> navcart()}>Cart {' '}
-            {countcartItems ? <button>{countcartItems}</button> : " "}</li>
+           
+         <li onClick={()=> navsupport()}>Support</li>
+           
 
-        </ul></nav>}
-    <button className='navbar' onClick={Shownavbar}><HiBars4/></button>
-   </header>
-  )
+                </ul>
+            </div>
+        </div>
+        
+    </nav>
+    )
+
+
+
+
+
+
+
+
+
+    // FIRST STYLING
+//     const {countcartItems}= props
+//     const [showNav, setShowNav]= useState(false)
+
+//     const Hidenav=()=>{
+//         showNav(true)
+//     }
+//     const navigate= useNavigate()
+
+    
+//     const navcat= ()=>{
+//         navigate("/menu")
+//         setShowNav(false)
+    
+//     }
+//     const navhome= ()=>{
+//         navigate("/")
+//         setShowNav(false)
+//     }
+//     const navfaq= ()=>{
+//         navigate("/faq")
+//         setShowNav(false)
+//     }
+//     const navsupport= ()=>{
+//         navigate("/support")
+//         setShowNav(false)
+//     }
+//     const navcart= ()=>{
+//         navigate("/cart")
+//         setShowNav(false)
+//     }
+
+//     const Shownavbar=()=>{
+//         setShowNav(!showNav)
+//         console.log("clicked nav")
+//     }
+
+
+//   return (
+//    <header className='header'>
+//   <button className='profile'><CgProfile/></button>
+  
+
+
+//     {showNav && <nav className='nav'>
+//         <ul className='links-container'>
+//             <li onClick={()=> navhome()}>Home</li>
+//             <li onClick={()=> navcat()}>Menu</li>
+//             <li onClick={()=> navfaq()}>FAQS</li>
+//             <li onClick={()=> navsupport()}>Support</li>
+//             <li onClick={()=> navcart()}>Cart {' '}
+//             {countcartItems ? <button>{countcartItems}</button> : " "}</li>
+
+//         </ul></nav>}
+//     <button className='navbar' onClick={Shownavbar}><HiBars4/></button>
+//    </header>
+//   )
 }
 
 export default Nav
